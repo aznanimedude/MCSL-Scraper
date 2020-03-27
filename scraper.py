7e8deab8308fdf1d34a55bceb10e551ea193e9ef
aznanimedude <aznanimedude06@gmail.com>
2020-03-27 19:25:10 -0400

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,59 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+
+    isQV = False
+    countdown = 0
+    
+    def handle_starttag(self, tag, attrs):
+        if self.isQV:
+            print("Encountered a start tag:", tag)
+#            for attr in attrs:
+#                print("     attr:",attr)
+        
+    
+    def handle_endtag(self, tag):
+        if self.isQV:
+            if self.countdown == 0:
+                print("DETECTED END OF QV DATA")
+                print("Encountered an end tag: ", tag)
+            #else:
+            #    self.countdown = self.countdown - 1
+            #    print(self.countdown)
+            
+        
+    
+    def handle_data(self, data):
+        
+        print("Encountered some data: ", data)
+
+        if data.endswith("(QV)"):
+            print("GO MARLINS")
+            self.isQV = True
+            self.countdown = 0
+        
+        if data == "QUAIL VALLEY":
+            print("GO MARLINS")
+            print("Detected Relay Data")
+            self.countdown = 3
+            #print(self.countdown)
+            self.isQV = True
+
+        if self.isQV == True:
+            if self.countdown == 0:
+                print("DETECTED QV DATA")
+                print("Encountered some data: ", data)
+                self.isQV = False
+            else:
+                self.countdown = self.countdown - 1
+                print(self.countdown)
+
+        
+
+with open(input("Enter html file name: ") + ".html") as myfile:
+    content = myfile.read()
+
+print(content)
+
+parser = MyHTMLParser()
+parser.feed(content)
