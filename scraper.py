d6a50175b0633e1a307ab1681ccb7627c247899b
aznanimedude <aznanimedude06@gmail.com>
2020-03-27 23:11:22 -0400

    got better output from html parser to correctly store data as a nested dictionary

--- scraper.py
+++ scraper.py
@@ -3,57 +3,103 @@ import html.parser
 class MyHTMLParser(html.parser.HTMLParser):
 
     isQV = False
-    countdown = 0
-    
+    relayCountdown = 0
+    teamData = {}
+    times = []
+    currentName = ''
+    currentEvent = ''
     def handle_starttag(self, tag, attrs):
         if self.isQV:
             print("Encountered a start tag:", tag)
 #            for attr in attrs:
 #                print("     attr:",attr)
-        
-    
+
+
     def handle_endtag(self, tag):
         if self.isQV:
-            if self.countdown == 0:
+            if self.relayCountdown == 0:
                 print("DETECTED END OF QV DATA")
                 print("Encountered an end tag: ", tag)
             #else:
             #    self.countdown = self.countdown - 1
             #    print(self.countdown)
-            
-        
-    
+
+
+
     def handle_data(self, data):
-        
-        print("Encountered some data: ", data)
+
+        #print("Encountered some data: ", data)
+
+        if data.startswith("Event"):
+            self.currentEvent = data
+            print("Found event", self.currentEvent)
+            self.teamData[self.currentEvent] = {}
 
         if data.endswith("(QV)"):
             print("GO MARLINS")
+            print("Start of QV data")
             self.isQV = True
-            self.countdown = 0
-        
+            self.currentName = data
+            print(self.currentEvent, self.currentName)
+            self.times = []
+            self.teamData[self.currentEvent][self.currentName] = self.times
+            self.relayCountdown = 3
+            print("added new swimmer to event", self.currentEvent)
+
+
         if data == "QUAIL VALLEY":
             print("GO MARLINS")
             print("Detected Relay Data")
-            self.countdown = 3
+            print("Start of QV data")
+            self.relayCountdown = 3
             #print(self.countdown)
             self.isQV = True
+            self.currentName = data
+            self.times = []
+            self.teamData[self.currentEvent][self.currentName] = self.times
+            return
+
 
         if self.isQV == True:
-            if self.countdown == 0:
+            if self.relayCountdown == 0:
                 print("DETECTED QV DATA")
                 print("Encountered some data: ", data)
+                self.teamData[self.currentEvent][self.currentName] = self.times
                 self.isQV = False
             else:
-                self.countdown = self.countdown - 1
-                print(self.countdown)
+                self.relayCountdown = self.relayCountdown - 1
+                if data.endswith("(QV)") == False:
+                    self.times.append(data)
+                    #print(self.countdown)
 
-        
 
-with open(input("Enter html file name: ") + ".html") as myfile:
+    def getData(self):
+        return self.teamData
+
+
+
+with open("testdata.html") as myfile:
     content = myfile.read()
 
 print(content)
 
 parser = MyHTMLParser()
 parser.feed(content)
+data = parser.getData()
+print("------------------------------")
+print("STARTING PRINTOUT")
+#print(data)
+for i in data:
+    print(i)
+    print(data[i])
+    for j in data[i]:
+        continue
+"""
+with open("output.txt","a") as outputfile:
+    for i in data:
+        outputfile.write(i)
+        outputfile.write("\n")
+        #for j in data[i]:
+         #   outputfile.write(j)
+          #  outputfile.write("\n")
+"""
